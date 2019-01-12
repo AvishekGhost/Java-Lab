b03453e49aa6b5c071e091cce34810666dea3317
AvishekGhost <33727239+AvishekGhost@users.noreply.github.com>
2019-01-12 20:57:59 +0530

    Update RoomDemo.java

--- No1/RoomDemo.java
+++ No1/RoomDemo.java
@@ -3,5 +3,10 @@ public class RoomDemo extends Room{
     RoomDemo(double Height, double Width, double Breadth){
         super(Height,Width,Breadth);
     }
+    // or
+    @Override
+    public double Volume(){
+        return super.Volume();
+    }
 
 }
