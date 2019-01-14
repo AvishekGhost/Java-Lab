91acec73fad201dc02a9d004d757dc1557a8f2a6
AvishekGhost <33727239+AvishekGhost@users.noreply.github.com>
2019-01-14 13:48:17 +0530

    Create Complex.java

---
+++ No6/Complex.java
@@ -0,0 +1,24 @@
+public class Complex{
+	add, multiply two complex numbers. 
+	private int Real;
+	private int Img;
+
+	Complex(){
+		Real = 0;
+		Img = 0;
+	}
+
+	Complex(int Real, int Img){
+		this.Real = Real;
+		this.Img = Img;
+	}
+
+	public void Display(){
+		System.out.println(Real + " + i" + Img);
+	}
+
+	public static Complex(){
+		
+	}
+
+}
