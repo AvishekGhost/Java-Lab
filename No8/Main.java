35727a1b20f9d7badbb8ced198077e6aa64f06f4
Avishek Mondal <33727239+AvishekGhost@users.noreply.github.com>
2019-01-17 20:59:32 +0530

    Create Main.java

---
+++ No8/Main.java
@@ -0,0 +1,36 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        Book []book = new Book[5];
+        Library lib = new Library();
+        for(int i = 0;i<5;i++){
+            int no = (int)( Math.random()*10);
+            String [] auth = new String[no];
+            for(int k =0; k<no;k++){
+                auth[k] = getSaltString();
+            }
+
+            book[i] = new Book(getSaltString() ,auth ,(int)( Math.random()*10000),(float)(Math.random()*1000),getSaltString());
+            lib.Add(book[i]);
+        }
+
+        System.out.println("Title\tAuthor\tPages\tPublisher\tPrice");
+        System.out.println("===================================================");
+        for(int i = 0;i<5;i++){
+            book[i].Display();
+        }
+    }
+
+    protected static String getSaltString() {
+        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        StringBuilder salt = new StringBuilder();
+        while (salt.length() < 5) {
+            int index = (int) ((float)Math.random() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+        String saltStr = salt.toString();
+        return saltStr;
+    }
+
+}
