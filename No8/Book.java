84316cfd2c429ee97bbfaaba5ccdaaf0f5592575
Avishek Mondal <33727239+AvishekGhost@users.noreply.github.com>
2019-01-17 20:59:55 +0530

    Create Book.java

---
+++ No8/Book.java
@@ -0,0 +1,32 @@
+class Book{
+    private String title;
+    private String [] author;
+    private int noOfPages;
+    private float price;
+    private String publisher;
+
+    public Book(String title, String []author, int noOfPages, float price, String publisher) {
+        this.author = new String[author.length];
+        this.title = title;
+        for (int i =0;i< author.length;i++){
+            this.author[i] = author[i];
+        }
+        this.noOfPages = noOfPages;
+        this.price = price;
+        this.publisher = publisher;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    void Display(){
+        System.out.print(title+"\t");
+        System.out.print(author[0]);
+        System.out.println("\t"+ noOfPages+ "\t" + publisher+ "\t\t" + price);
+        for(int i =1;i<author.length;i++) {
+            System.out.print("\t\t" + author[i]+"\n");
+        }
+        System.out.println("___________________________________________________");
+    }
+}
