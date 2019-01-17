5378ed6aa8a7d359b589bc7047b37bd7f9b11846
Avishek Mondal <33727239+AvishekGhost@users.noreply.github.com>
2019-01-17 21:00:20 +0530

    Create Library.java

---
+++ No8/Library.java
@@ -0,0 +1,26 @@
+public class Library {
+    Book books[]= new Book[5];
+    int index = -1;
+
+    void Add(Book src){
+        if(this.index > 5) {
+            System.out.println("max books added");
+            return;
+        }
+        else {
+            this.index++;
+            books[this.index] = src;
+        }
+    }
+    void Remove(Book src,String title){
+        for(int i =0;i<this.index;i++){
+            if(books[i].getTitle() == title ){
+                for(int j = i;j<this.index;j++){
+                    books[j] = books[j+1];
+                }
+                this.index--;
+            }else
+                System.out.println("Nothing to delete");
+        }
+    }
+}
